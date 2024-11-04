49a5fa4e0d86c1e9e0f5b287282650f267604d9f
LuksFavs <lucasffas@dcc.ufrj.br>
2024-11-03 22:58:45 -0300

    algo finished and problem in cities distance found, Belém and Boa Vista are not the real ones.

--- rotas.py
+++ rotas.py
@@ -33,14 +33,13 @@ def initializer():
 class ANTS:
     def __init__(self, initialLoc):
         self.caminho = [initialLoc]
-        self.pops = pops[index[initialLoc]]
+        self.localPops = pops[index[initialLoc]]
 
     def calcularCaminho(self):
-        self.totalPops = self.pops
+        self.totalPops = self.localPops
         self.percentPops = 0
         self.dist = 0
-        self.popsDist = 0
-        self.popsDistPercent = 0
+        self.obj = 0
         self.sumOfFer = 0 
         self.citiesVisited = 1
 
@@ -51,7 +50,7 @@ class ANTS:
             for i in range(len(localFer)):
                 if cidIndex[i] in self.caminho:
                     localFer[i] = 0
-            atratividade = np.divide(pops+self.totalPops, self.dist+dis[lugar], out=np.zeros_like(pops), where=dis[lugar]!=0)
+            atratividade = np.divide(pops+self.totalPops,self.dist+dis[lugar]*17000, out=np.zeros_like(pops), where=dis[lugar]!=0)
             aux = atratividade * localFer
             if np.sum(aux) == 0:
                 break
@@ -63,55 +62,53 @@ class ANTS:
                 if aux2 < aux3:
                     self.totalPops+=pops[i]
                     self.dist+=dis[lugar][i]
-                    self.popsDist=self.totalPops / self.dist
+                    self.obj =  self.totalPops / self.dist  
                     self.caminho.append(cidIndex[i])
                     self.percentPops = self.totalPops/totalPops
                     self.citiesVisited+=1
-                    self.popsDistPercent = self.popsDist*(1+(self.percentPops+(self.citiesVisited/27)))
                     for k in self.caminho[1:]:
                         self.sumOfFer = localFer[index[k]]
                     self.sumOfFer/=len(self.caminho)
                     break
-            if self.citiesVisited>10 or self.percentPops>0.6:
+            if bestCaminho != [] and self.obj > bestCaminho[0][0][0] and self.citiesVisited > bestCaminho[0][0][1] or self.percentPops>0.9 :
                 break
 
 
 
 def getValue(e):
-    return e[0]
+    return e[0][0]
 
 ## Main
 NUMITER, NUMANTS, CITY, FERO,EVAP, colonia = initializer()
 bestCaminho = []
-
 for i in range(NUMITER):
-    if i %100 == 0:
+    if i %1000 == 0:
         print(i)
     for ant in colonia:
         ant.calcularCaminho()
         if bestCaminho == []:
-            bestCaminho.append([ant.popsDistPercent, ant.popsDist, ant.caminho.copy(), ant.sumOfFer])
+            bestCaminho.append([[ant.obj, ant.citiesVisited], ant.caminho.copy(),ant.citiesVisited, ant.sumOfFer])
         else:
-            teste = False
-            for resultados in bestCaminho:
-                if [ant.popsDist, ant.caminho] == [resultados[0], resultados[2]]:
-                    teste=True
+            for i in range(len(bestCaminho)):
+                if bestCaminho[i][1] == ant.caminho:
                     break
-            if teste == True:
-                bestCaminho.append([ant.popsDistPercent, ant.popsDist, ant.caminho.copy(), ant.sumOfFer])
+                elif i == len(bestCaminho)-1:
+                    bestCaminho.append([[ant.obj, ant.citiesVisited], ant.caminho.copy(), ant.sumOfFer])
+
         ant.caminho = [CITY]
-    bestCaminho.sort(reverse = True, key=getValue)
-    bestCaminho = bestCaminho[0:3]
+    bestCaminho.sort(reverse=True, key=getValue)
+    aux = bestCaminho.copy()
+    bestCaminho = [bestCaminho[0]]
     pares = []
 
     for resultados in bestCaminho:
         aux2 = []
-        for individuos in range(len(resultados[2])):
-            if individuos == len(resultados[2])-1:
-                pares.append([aux2.copy(), resultados[3]])
+        for individuos in range(len(resultados[1])):
+            if individuos == len(resultados[1])-1:
+                pares.append([aux2.copy(), resultados[2]])
                 break
             else:
-                aux = [resultados[2][individuos], index[resultados[2][individuos+1]]]
+                aux = [resultados[1][individuos], index[resultados[1][individuos+1]]]
                 aux2.append(aux.copy())
 
     for cidade in capitais_estaduais:
